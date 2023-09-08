652a4aa7e6d3aaab26c3317c92f353ca83edc20f
justinndidit <favourolasoji@student.oauife.edu.ng>
2023-09-08 04:31:55 +0100

    Updated  to fit HNG  specifications

--- controller.js
+++ controller.js
@@ -12,7 +12,11 @@ const daysOfWeek = [
 
 const getUser = async function (req, res) {
   const { slack_name, track } = req.query;
+  const day = new Date().getDay(); //new Date().getDay();
+
   if (!slack_name || !track) {
+    Data.current_day = daysOfWeek[day];
+    Data.utc_time = new Date().toISOString().slice(0, -5) + "Z";
     return res.status(200).send(Data);
   }
 
@@ -21,7 +25,7 @@ const getUser = async function (req, res) {
       users.slack_name.split(" ").join("_").toLocaleLowerCase() ===
         slack_name && users.track === track
   );
-  user.current_day = daysOfWeek[new Date().getDay()];
+  user.current_day = daysOfWeek[day];
   user.utc_time = new Date().toISOString().slice(0, -5) + "Z";
 
   res.status(200).send(user);
