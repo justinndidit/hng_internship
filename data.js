e4258166dcf95a519873789002b19907f629df75
justinndidit <favourolasoji@student.oauife.edu.ng>
2023-09-08 02:11:59 +0100

    Updated my data to fit specifications

--- data.js
+++ data.js
@@ -4,8 +4,9 @@ module.exports = [
     current_day: new Date().toLocaleString("en-US", { weekday: "long" }),
     utc_time: new Date().toISOString().slice(0, -5) + "Z",
     track: "backend",
-    github_file_url: "https://github.com/username/repo/blob/main/file_name.ext",
-    github_repo_url: "https://github.com/username/repo",
+    github_file_url:
+      "https://github.com/justinndidit/hng_internship/blob/main/server.js",
+    github_repo_url: "https://github.com/justinndidit/hng_internship",
     status_code: 200,
   },
 ];
